7d962e4ef4d6975c18bf2473c4d85a7686ecb7ad
Alaa Ahmed <alaaahmedtaha123@gmail.com>
2026-09-07 14:10:08 +0300

    Integrate MediatR and FluentValidation in DependencyInjection
    Added MediatR and FluentValidation services for better request handling and validation.

--- ECommerce.Application/DependencyInjection/ApplicationServiceRegistration.cs
+++ ECommerce.Application/DependencyInjection/ApplicationServiceRegistration.cs
@@ -1,6 +1,10 @@
 using ECommerce.Application.Interfaces.Services;
 using ECommerce.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+using ECommerce.Application.Common.Behaviors;
 
 namespace ECommerce.Application.DependencyInjection;
 
@@ -8,6 +12,13 @@ public static class ApplicationServiceRegistration
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        }); //adding mediatR
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddScoped<IProductService, ProductService>();
         services.AddScoped<ICustomerService, CustomerService>();
         services.AddScoped<IOrderService, OrderService>();
