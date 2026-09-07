0810cf9478f85ff8b6bc219ee970c7c2ffed4081
Alaa Ahmed <alaaahmedtaha123@gmail.com>
2026-09-07 14:06:14 +0300

    Editing CreateCustomerCommand and fixing some errors

--- ECommerce.Application/Features/Customers/Commands/CreateCustomer/CreateCustomerCommand.cs
+++ ECommerce.Application/Features/Customers/Commands/CreateCustomer/CreateCustomerCommand.cs
@@ -1,13 +1,6 @@
 ﻿using ECommerce.Application.DTOs.Customers;
-using MediatR;                               
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using MediatR;
 
-namespace ECommerce.Application.Features.Customers.Commands.CreateCustomer
-{
-    public record CreatesCustomerCommand(string Fullname, string Email, bool isVip) : IRequest<CustomerResponse>
-}
+namespace ECommerce.Application.Features.Customers.Commands.CreateCustomer;
+
+public record CreateCustomerCommand(string FullName, string Email, bool IsVip) : IRequest<CustomerResponse>;
